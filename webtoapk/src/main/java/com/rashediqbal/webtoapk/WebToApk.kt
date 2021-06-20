7819390d92f896da38bbd9becfc6303c1563d1a7
Rashed Iqbal <rashediqbalripon@gmail.com>
2021-06-20 22:57:10 +0600

    Add some features

--- webtoapk/src/main/java/com/rashediqbal/webtoapk/WebToApk.kt
+++ webtoapk/src/main/java/com/rashediqbal/webtoapk/WebToApk.kt
@@ -1,11 +1,23 @@
 package com.rashediqbal.webtoapk
 
 import android.app.Activity
+import android.app.DownloadManager
 import android.content.Context
+import android.content.Context.DOWNLOAD_SERVICE
+import android.content.pm.PackageManager
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
+import android.net.Uri
 import android.os.Build
+import android.os.Environment
+import android.webkit.CookieManager
+import android.webkit.URLUtil
+import android.webkit.WebView
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class WebToApk (private val context: Context){
 
@@ -51,5 +63,55 @@ class WebToApk (private val context: Context){
         val alertDialog = dialogBuilder.create()
         alertDialog.show()
     }
+
+    public fun checkPermission(permission: String,requestCode:Int){
+        if(ContextCompat.checkSelfPermission(context , permission) == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(context as Activity, arrayOf(permission),requestCode)
+        }
+    }
+
+    public fun enableDownload(webView:WebView){
+        webView.setDownloadListener { url, userAgent, contentDisposition, mimetype, _ ->
+
+            val request: DownloadManager.Request = DownloadManager.Request(Uri.parse(url))
+            request.setMimeType(mimetype)
+            request.addRequestHeader("cookie", CookieManager.getInstance().getCookie(url))
+            request.addRequestHeader("User-Agent",userAgent)
+            request.setDescription("Download file...")
+            request.allowScanningByMediaScanner()
+            request.setTitle(URLUtil.guessFileName(url,contentDisposition,mimetype))
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,URLUtil.guessFileName(url,contentDisposition,mimetype))
+            val downloadManager:DownloadManager = context.getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+            downloadManager.enqueue(request)
+            Toast.makeText(context,"Downloading File",Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
+    public fun enableProgress(startProgress:ProgressBar,progressBar:ProgressBar,newProgress:Int,isStart:Boolean):Boolean {
+        progressBar.progress = newProgress
+        startProgress.progress = newProgress
+
+        if(isStart){
+            if(newProgress <100 && startProgress.visibility == ProgressBar.GONE){
+                startProgress.visibility = ProgressBar.VISIBLE
+            }
+            if(newProgress == 100 && startProgress.visibility == ProgressBar.VISIBLE){
+                startProgress.visibility = ProgressBar.GONE
+            }
+
+        }
+        if(!isStart){
+            if(newProgress <100 && progressBar.visibility == ProgressBar.GONE){
+                progressBar.visibility = ProgressBar.VISIBLE
+            }
+            if(newProgress == 100){
+                progressBar.visibility = ProgressBar.GONE
+            }
+        }
+        return false
+    }
+
 
 }
